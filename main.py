ca7ad1d40002fe31d1d65bfdea741ca9b6d110b2
Aditya Gupta <adityagupta2087@gmail.com>
2026-01-12 22:08:38 +0530

    completed chapter 1 LLMs lesson 4 user input

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model='gemini-2.5-flash',
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents=args.user_prompt
 )
 
 print(response.text)
