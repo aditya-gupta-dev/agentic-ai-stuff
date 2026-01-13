6c70ecd219ed6e662622ca3b20e56cca3019b37d
Aditya Gupta <adityagupta2087@gmail.com>
2026-01-13 21:51:32 +0530

    completed chapter 1 LLMs lesson 6 multiple messages

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import argparse
 from dotenv import load_dotenv
-from google import genai
+from google import genai 
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,9 +12,11 @@ args = parser.parse_args()
 
 client = genai.Client(api_key=api_key)
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 response = client.models.generate_content(
     model='gemini-2.5-flash',
-    contents=args.user_prompt
+    contents=messages
 )
 
 print(response.text)
