ffbcad951737fa50a628eae7b52134a40e18bef6
Aditya Gupta <adityagupta2087@gmail.com>
2026-01-14 20:51:43 +0530

    completed chapter 1 lesson 7 Verbose output

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
 args = parser.parse_args()
 
 client = genai.Client(api_key=api_key)
@@ -19,7 +21,9 @@ response = client.models.generate_content(
     contents=messages
 )
 
-print(response.text)
-
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
+if args.verbose: 
+    print("User prompt: ", args.user_prompt)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+else: 
+    print(response.text)
